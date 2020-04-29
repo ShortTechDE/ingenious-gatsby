ca787d1c9881d8c976f5d19476fc59da726f402e
tobimori <tobias@moeritz.cc>
2020-04-29 19:47:46 +0200

    🔧 update siteconfig

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://shorttech.de`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 9, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `ShortTech`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Wir berichten Communitynah über die Neuerungen der Technik- und Medienwelt... und jetzt auch im komplettem Redesign!`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
+    shortTitle: `ShortTech`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `logo.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    backgroundColor: `#111111`, // Used for Offline Manifest
+    themeColor: `#1688A7`, // Used for Offline Manifest
 }
