4750408b0ae6eded00069412946208a993a8fe5a
tobimori <tobias@moeritz.cc>
2020-04-29 19:40:01 +0200

    🚧 take over 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,16 +3,18 @@ import { Link } from 'gatsby'
 import { Layout } from '../components/common'
 
 const NotFoundPage = () => (
-    <Layout>
-        <div className="container">
-            <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
-                <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
-                </section>
-            </article>
-        </div>
-    </Layout>
+  <Layout>
+    <header class="page-header error small">
+      <div class="content container">
+        <h1 class="headline">Seite nicht gefunden</h1>
+      </div>
+      <figure class="wave"></figure>
+    </header>
+
+    <section class="error container">
+      <Link to="/">Zurück zum Start</Link>
+    </section>
+  </Layout>
 )
 
 export default NotFoundPage
